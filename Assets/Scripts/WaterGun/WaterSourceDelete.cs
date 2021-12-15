9cb63d9f2c98e609ce5efc18a568ba7e86bcff42
niigawa <Niigawa.Sosuke2022@gmail.com>
2021-12-15 13:52:27 +0900

    Add HP Down Effect

---
+++ Assets/Scripts/WaterGun/WaterSourceDelete.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSourceDelete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "ColdWater")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
